21c8736a0b1c3230fc5da5398b92a700c6f020da
fet012 <falolachristiana446@gmail.com>
2024-12-16 12:54:46 +0100

    finalizing pwa

--- vite.config.js
+++ vite.config.js
@@ -15,14 +15,15 @@ export default defineConfig({
         theme_color: "#800080 ",
         background_color: "#eae6f8",
         display: "standalone",
+        start_url: "/",
         icons: [
           {
-            src: "icon-192x192.png",
+            src: "icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icon-512x512.png",
+            src: "icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
